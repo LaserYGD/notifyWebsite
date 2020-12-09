69a5b76927e29f714f0c71eee198de7caa4b7181
zed <zed0h21@gmail.com>
2020-12-09 22:01:05 +0300

    syntax fixes again and started using Head

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import NavBar from "../components/navBar";
 import Image from "next/image";
 import styled from "styled-components";
+import { render } from "react-dom";
 
 const flexDiv = styled.div`
   display: flex;
@@ -13,42 +14,48 @@ const flex110 = styled.div`
 
 export default function Home() {
   return (
-    <div>
-      <title>TwitchNotify - Track Twitch Catagories by views and set conditions!</title>
-      <meta name="description" content="Create custom conditions for twitch games/Catagories based on views and get notified when they're true!">
-        {" "}
-      </meta>
-      <meta name="keywords" content="Twitch, Utility, Conditions, Notifications"></meta>
+    <>
+      <Head>
+        <title>TwitchNotify - Track Twitch Catagories by views and set conditions!</title>
+        <meta
+          name="description"
+          content="Create custom conditions for twitch games/Catagories based on views and get notified when they're true!"
+        ></meta>
 
-      <NavBar />
+        <meta name="keywords" content="Twitch, Utility, Conditions, Notifications"></meta>
+      </Head>
 
-      <div id="flex-container">
-        <div className="flex-item first-page-text" id="first-page-text">
-          {" "}
-          <p>
-            Imagine a world where you can set a condition on any catagory/game for Twitch. <br></br> A world where you'd get a notification when that
-            condition is true. <br></br>
-            <br></br> The impossible has become possible, the greatest random application that no sane person would spend their time on making is
-            finally here. <br></br> <br></br> We call it, TwitchNotify. <br></br>
-            <span id="why">Because it notifies you, get it? I admit, took me two seconds and one braincell to think of this name.</span>
-          </p>
-        </div>
+      <div>
+        <NavBar />
 
-        <div className="flex-item view-image">
-          <Image
-            src="/conditionSettings.png"
-            alt="picture of the program's condition settings window"
-            id="conditionImage"
-            width={450}
-            height={420}
-          ></Image>
-          <p className="light view-image">
-            Custom view conditions<br></br> Cooldown system to prevent notification spam <br></br> Almost all catagories supported <br></br>Our server
-            tracks and updates all catagories continuously<br></br>Your games & settings are locally saved
-          </p>
+        <div id="flex-container">
+          <div className="flex-item first-page-text" id="first-page-text">
+            {" "}
+            <p>
+              Imagine a world where you can set a condition on any catagory/game for Twitch. <br></br> A world where you'd get a notification when
+              that condition is true. <br></br>
+              <br></br> The impossible has become possible, the greatest random application that no sane person would spend their time on making is
+              finally here. <br></br> <br></br> We call it, TwitchNotify. <br></br>
+              <span id="why">Because it notifies you, get it? I admit, took me two seconds and one braincell to think of this name.</span>
+            </p>
+          </div>
+
+          <div className="flex-item view-image">
+            <Image
+              src="/conditionSettings.png"
+              alt="picture of the program's condition settings window"
+              id="conditionImage"
+              width={450}
+              height={420}
+            ></Image>
+            <p className="light view-image">
+              Custom view conditions<br></br> Cooldown system to prevent notification spam <br></br> Almost all catagories supported <br></br>Our
+              server tracks and updates all catagories continuously<br></br>Your games & settings are locally saved
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
