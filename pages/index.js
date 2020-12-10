01027f85f9b2bf75051ed7b9bf66d73e8f9bea21
zed <zed0h21@gmail.com>
2020-12-10 19:42:05 +0300

    ..

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,14 @@ import NavBar from "../components/navBar";
 import Image from "next/image";
 import { render } from "react-dom";
 
+let a = 2;
+
+if (a == 2) {
+  a = 5;
+}
+
+const CCCC = 5213123;
+
 //-
 export default function Home() {
   return (
