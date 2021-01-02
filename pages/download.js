239f241666bd38995a94501202b1f4d642312972
zed <zed0h21@gmail.com>
2021-01-02 14:07:11 +0300

    changes..

--- pages/download.js
+++ pages/download.js
@@ -6,19 +6,27 @@ export default function download() {
     <div>
       <NavBar />
       <p className="light">Mac & Linux support is coming soon!</p>
-      <p className="light">this will download the latest version by default</p>
+      <p className="light">
+        TwitchNotify is a 'hmm' hobby project. Thus isn't signed (requires $$). Smartscreen or Antivirus will give you a false positive.
+      </p>
       <p className="light">Click on the image to download</p>
+      <div>
+        <br></br>
+        <br></br>
+        <br></br>
+      </div>
       <div id="downloadWindows">
         <h1 className="downloadText">Download for windows</h1>
         <a href="https://github.com/LaserYGD/twitchNotify/releases/download/release/twitchNotify.Setup.0.8.0.exe" target="_blank">
           <Image src="/windowsLogo.png" alt="picture of the program's condition settings window" id="conditionImage" width={180} height={150}></Image>
         </a>
       </div>
-
-      <div id="downloadLinux">
-        <h1 className="downloadText">Download for Linux</h1>
-        <Image src="/linuxLogo3.png" alt="picture of the program's condition settings window" id="conditionImage" width={200} height={150}></Image>
-      </div>
     </div>
   );
 }
+
+//the the pure <br> tag makes you want to shoot me. Then unload an entire mag on me to make sure it was done. I know.
+/* <div id="downloadLinux">
+        <h1 className="downloadText">Download for Linux</h1>
+        <Image src="/linuxLogo3.png" alt="picture of the program's condition settings window" id="conditionImage" width={200} height={150}></Image>
+      </div>*/
